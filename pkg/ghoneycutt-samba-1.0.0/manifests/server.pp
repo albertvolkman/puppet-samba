17ca245e992ed009123a8b6074de482ebf678f03
Peter Drake <pdrake@allplayers.com>
2012-01-11 11:12:34 -0500

    Build puppet module to latest specs.

--- pkg/ghoneycutt-samba-1.0.0/manifests/server.pp
+++ pkg/ghoneycutt-samba-1.0.0/manifests/server.pp
@@ -8,8 +8,34 @@
 #
 class samba::server inherits samba {
 
-    include generic
-
     package { "samba": }
+
+    file { "/usr/bin/smbsync":
+       owner => root,
+       group => root,
+       mode => 755,
+       source => "puppet:///modules/samba/smbsync",
+       require => Package["samba"]
+    }
+
+    file { "/etc/samba/smb.conf":
+         owner => root,
+         group => root,
+         mode => 644,
+         content => template('samba/smb.conf.erb'),
+         require => Package["samba"],
+         notify => Exec["reload-samba"],
+    }
+
+    exec {"samba-user-sync":
+        command => "/usr/bin/smbsync",
+        path => "/usr/bin:/usr/sbin:/bin:/usr/local/bin:/sbin",
+        require => File["/usr/bin/smbsync"],
+    }
+
+    exec { "reload-samba":
+         command => "/sbin/restart smbd",
+         refreshonly => true,
+    }
 
 } # class samba
