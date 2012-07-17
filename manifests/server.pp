320d2289e066e011f7cbe73e1dc9658229752ee4
Peter Drake <pdrake@allplayers.com>
2012-07-17 14:02:50 -0400

    [#32863803] Refactor samba module to define services.

--- manifests/server.pp
+++ manifests/server.pp
@@ -8,34 +8,41 @@
 #
 class samba::server inherits samba {
 
-    package { "samba": }
+  package { "samba": }
 
-    file { "/usr/bin/smbsync":
-       owner => root,
-       group => root,
-       mode => 755,
-       source => "puppet:///modules/samba/smbsync",
-       require => Package["samba"]
-    }
+  file { "/usr/bin/smbsync":
+   owner => root,
+   group => root,
+   mode => 755,
+   source => "puppet:///modules/samba/smbsync",
+   require => Package["samba"]
+  }
 
-    file { "/etc/samba/smb.conf":
-         owner => root,
-         group => root,
-         mode => 644,
-         content => template('samba/smb.conf.erb'),
-         require => Package["samba"],
-         notify => Exec["reload-samba"],
-    }
+  file { "/etc/samba/smb.conf":
+     owner => root,
+     group => root,
+     mode => 644,
+     content => template('samba/smb.conf.erb'),
+     require => Package["samba"],
+     notify => Service["smbd"],
+  }
 
-    exec {"samba-user-sync":
-        command => "/usr/bin/smbsync",
-        path => "/usr/bin:/usr/sbin:/bin:/usr/local/bin:/sbin",
-        require => File["/usr/bin/smbsync"],
-    }
+  exec {"samba-user-sync":
+    command => "/usr/bin/smbsync",
+    path => "/usr/bin:/usr/sbin:/bin:/usr/local/bin:/sbin",
+    require => File["/usr/bin/smbsync"],
+  }
 
-    exec { "reload-samba":
-         command => "/sbin/restart smbd; /sbin/restart nmbd",
-         refreshonly => true,
-    }
+  service { "smbd":
+    enable  => true,
+    ensure  => running,
+    require => Package["samba"],
+    notify  => Service["nmbd"],
+  }
+
+  service { "nmbd":
+    enable  => true,
+    ensure  => running,
+  }
 
 } # class samba
