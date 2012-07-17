cbbe1f6330765b8b58b7ec4554e3e1973167027e
Peter Drake <pdrake@allplayers.com>
2012-07-17 14:10:23 -0400

    [#32863803] Additional formatting cleanup.

--- manifests/server.pp
+++ manifests/server.pp
@@ -11,25 +11,25 @@ class samba::server inherits samba {
   package { "samba": }
 
   file { "/usr/bin/smbsync":
-   owner => root,
-   group => root,
-   mode => 755,
-   source => "puppet:///modules/samba/smbsync",
-   require => Package["samba"]
+    owner   => root,
+    group   => root,
+    mode    => 755,
+    source  => "puppet:///modules/samba/smbsync",
+    require => Package["samba"]
   }
 
   file { "/etc/samba/smb.conf":
-     owner => root,
-     group => root,
-     mode => 644,
-     content => template('samba/smb.conf.erb'),
-     require => Package["samba"],
-     notify => Service["smbd"],
+     owner    => root,
+     group    => root,
+     mode     => 644,
+     content  => template('samba/smb.conf.erb'),
+     require  => Package["samba"],
+     notify   => Service["smbd"],
   }
 
   exec {"samba-user-sync":
     command => "/usr/bin/smbsync",
-    path => "/usr/bin:/usr/sbin:/bin:/usr/local/bin:/sbin",
+    path    => "/usr/bin:/usr/sbin:/bin:/usr/local/bin:/sbin",
     require => File["/usr/bin/smbsync"],
   }
 
