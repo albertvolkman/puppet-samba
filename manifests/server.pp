f5d2733980d23f6a864910a97efe9e54a0fe67d1
Peter Drake <pdrake@allplayers.com>
2012-08-21 10:30:38 -0400

    [#34659447] Change samba user sync to only occur when refreshed.

--- manifests/server.pp
+++ manifests/server.pp
@@ -8,14 +8,18 @@
 #
 class samba::server inherits samba {
 
-  package { "samba": }
+  package { "samba":
+    ensure  => installed,
+    notify  => Exec["samba-user-sync"],
+  }
 
   file { "/usr/bin/smbsync":
     owner   => root,
     group   => root,
     mode    => 755,
     source  => "puppet:///modules/samba/smbsync",
-    require => Package["samba"]
+    require => Package["samba"],
+    notify  => Exec["samba-user-sync"],
   }
 
   file { "/etc/samba/smb.conf":
@@ -28,9 +32,10 @@ class samba::server inherits samba {
   }
 
   exec {"samba-user-sync":
-    command => "/usr/bin/smbsync",
-    path    => "/usr/bin:/usr/sbin:/bin:/usr/local/bin:/sbin",
-    require => File["/usr/bin/smbsync"],
+    command     => "/usr/bin/smbsync",
+    path        => "/usr/bin:/usr/sbin:/bin:/usr/local/bin:/sbin",
+    require     => File["/usr/bin/smbsync"],
+    refreshonly => true,
   }
 
   service { "smbd":
